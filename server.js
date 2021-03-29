60d4a6d14209166ba53bc00841008d95328c27e2
Seakleang <ahjokatooskii25@gmail.com>
2021-03-29 21:23:06 +0700

    add view

--- server.js
+++ server.js
@@ -1,1 +1,37 @@
-console.log("It's working.")
+
+// require
+const http = require("http");
+const express = require("express");
+const socket = require("socket.io");
+
+// init variable
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+
+const port = process.env.port || 80;
+
+// response path
+app.set("view engine", "ejs");
+app.get("", (req, res) => {
+    res.render("home");
+});
+
+// socket event
+io.on("connection", socket => {
+    socket.on("online", data => {
+        console.log("online event: " + data);
+    });
+
+    socket.on("disconnect", () => {
+        socket.emit("connection_count_change", io.engine.clientsCount);
+        console.log("offline event");
+    });
+    socket.emit("connection_count_change", io.engine.clientsCount);
+    console.log("connection event");
+});
+
+// start the server
+server.listen(port, () => console.log("server is running on port 80"));
+
+console.log("It's working.");
